430a5f6b9d7c2e4576038b1582720047ff3a8773
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-05-31 14:33:48 -0700

    implementing revcomp stuff

---
+++ layers/convolutional.py
@@ -0,0 +1,44 @@
+from keras.layers.convolutional import Conv1D
+from keras.utils import conv_utils
+from keras import backend as K
+
+
+class RevCompConv1D(Conv1D):
+
+    def compute_output_shape(self, input_shape):
+        length = conv_utils.conv_output_length(input_shape[1],
+                                               self.kernel_size[0],
+                                               padding=self.padding,
+                                               stride=self.strides[0])
+        return (input_shape[0], length, 2*self.filters)
+
+    def call(self, inputs):
+        #create a rev-comped kernel.
+        #kernel shape is (width, input_channels, filters)
+        #Rev comp is along both the length (dim 0) and input channel (dim 1)
+        #axes; that is the reason for ::-1, ::-1 in the first and second dims.
+        #The rev-comp of channel at index i should be at index -i
+        #This is the reason for the ::-1 in the last dim.
+        revcomp_kernel =\
+            K.concatenate([self.kernel,
+                           self.kernel[::-1,::-1,::-1]],axis=-1)
+        if (self.use_bias):
+            revcomp_bias = K.concatenate([self.bias,
+                                          self.bias[::-1]], axis=-1)
+
+        outputs = K.conv1d(inputs, revcomp_kernel,
+                           strides=self.strides[0],
+                           padding=self.padding,
+                           data_format=self.data_format,
+                           dilation_rate=self.dilation_rate[0])
+
+        if self.use_bias:
+            outputs += K.bias_add(outputs,
+                                  revcomp_bias,
+                                  data_format=self.data_format)
+
+        if (self.activation is not None):
+            outputs = self.activation(outputs)
+        return outputs
+
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='keras_genomics',
+      version='2.0.0',
+      description='Keras Deep Learning for Genomics layers',
+      author='Kundaje Lab',
+      author_email='avanti.shrikumar@gmail.com',
+      url='https://github.com/farizrahman4u/keras-genomics',
+      license='MIT',
+      install_requires=['keras'],
+      extras_require={
+          'h5py': ['h5py'],
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'pytest-xdist',
+                    'pytest-cov'],
+      },
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
