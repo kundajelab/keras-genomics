1024de784b446ab9936f84989c651b94896e01e0
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-05-31 20:28:37 -0700

    organizing into package

---
+++ keras_genomics/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from . import layers
+
+__version__ = '2.0.0'
---
+++ keras_genomics/layers/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from .core import *
+from .convolutional import *
+from .normalization import *
