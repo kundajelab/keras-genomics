6b6c1a9d123f91193e5311ccd22d169a6c9d0733
kttian <19katherinet@students.harker.org>
2018-06-25 21:47:33 -0700

    fixed K.binary_crossentropy call

--- keras_genomics/losses.py
+++ keras_genomics/losses.py
@@ -4,5 +4,5 @@ from keras import backend as K
 
 def ambig_binary_crossentropy(y_true,y_pred):
         non_ambig = K.cast((y_true > -0.5),'float32')
-        return K.mean(K.binary_crossentropy(y_pred, y_true)
+        return K.mean(K.binary_crossentropy(y_true, y_pred)
                       *non_ambig, axis=-1)
