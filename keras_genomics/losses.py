33a67edc045aee6a7b7c4f40e2ca5e987134524b
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-11-21 05:15:58 +0000

    hybrid_loss

--- keras_genomics/losses.py
+++ keras_genomics/losses.py
@@ -17,15 +17,37 @@ def regression_and_classification(y_true, y_pred):
     y_true_binarized = K.cast(K.greater_equal(y_true, 1.0), K.floatx()) 
     hard_sigmoid_pred = K.hard_sigmoid(y_pred)
     linearized_hard_sigmoid_pred = (0.2*y_pred) + 0.5
-    linearized_hard_sigmoid_pred = (
-        K.cast(K.greater(linearized_hard_sigmoid_pred, 0.0),
-               K.floatx())*linearized_hard_sigmoid_pred)
     binary_crossentropy_loss = K.mean(K.binary_crossentropy(
                                         output=hard_sigmoid_pred,
                                         target=y_true_binarized), axis=-1)
-    mse_loss = K.mean(2.71*K.square(linearized_hard_sigmoid_pred - y_true),
+    mse_loss = K.mean(2.71*K.square(linearized_hard_sigmoid_pred - y_true)
+                          *K.cast(K.greater(y_true, 1.0), K.floatx()),
                       axis=-1)
     return binary_crossentropy_loss + mse_loss 
+
+
+def hybrid_loss(y_true,y_pred):
+    #y_true and y_pred are structured as follows:
+    #first dimension is sample,
+    #second dimension has length 2; first entry in y_pred is the *logit*
+    # of the sigmoid (we will put it through the sigmoid in the loss func and
+    # then apply binary crossentropy)
+    # second entry in y_pred is the predicted value for the regression (we
+    # will apply a mean-squared error loss to this)
+
+    binary_task_predicted_logits = y_pred[:,0:1]
+    binary_y_true = y_true[:,0:1]
+    regression_y_true = y_true[:,1:2]
+    regression_task_predictions = y_pred[:,1:2]
+    binary_task_crossentropy_loss = K.mean(K.binary_crossentropy(
+        target=binary_y_true, output=binary_task_predicted_logits,
+        from_logits=True),axis=-1)
+    regression_weight = binary_y_true
+    regression_loss = K.mean(K.square(regression_task_predictions -
+                                      regression_y_true)*regression_weight,
+                             axis=-1)
+    
+    return binary_task_crossentropy_loss + regression_loss
 
 
 def weighted_regression(y_true,y_pred):
@@ -41,7 +63,7 @@ def weighted_regression(y_true,y_pred):
     regression_task_predictions = y_pred[:,1:2]
     binary_task_crossentropy_loss = K.mean(K.binary_crossentropy(
         target=y_true[:,0:1], output=binary_task_predicted_logits,
-        from_logits=True))
+        from_logits=True),axis=-1)
     positives_upweight_factor = K.sum(1.0-y_true[:,0])/K.sum(y_true[:,0])
     regression_weight = y_true[:,0:1]*(positives_upweight_factor) + (1-y_true[:,0:1])*1.0
     regression_loss = K.mean(K.square(regression_task_predictions -
